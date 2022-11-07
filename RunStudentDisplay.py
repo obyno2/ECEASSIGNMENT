9ba67e07a5a176b39dca42fcf39844c4fc2ee037
obyno2 <66160711+obyno2@users.noreply.github.com>
2022-11-07 13:49:23 -0800

    Create RunStudentDisplay.py

---
+++ RunStudentDisplay.py
@@ -0,0 +1,8 @@
+from core.StudentDisplay import StudentDisplay
+
+"""
+This is the program used to run the StudentDisplay.py program
+"""
+
+student_display = StudentDisplay()
+student_display.run()
