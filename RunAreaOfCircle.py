c9a760a436bca7503d1b589a11d155026a454c4f
obyno2 <66160711+obyno2@users.noreply.github.com>
2022-11-07 13:48:05 -0800

    Create RunAreaOfCircle.py

---
+++ RunAreaOfCircle.py
@@ -0,0 +1,9 @@
+from core.CicrleAreaCalculator import CircleAreaCalculator
+
+
+"""
+This is the program used to run the CircleAreaCalculator.py software
+"""
+
+circleAreaCalculator = CircleAreaCalculator()
+circleAreaCalculator.run()
